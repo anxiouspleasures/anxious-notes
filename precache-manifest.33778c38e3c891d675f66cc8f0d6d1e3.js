c36b1b20dc3aa4b04ea89d54c98069b613d525a1
Mike Keith <83459501+actsasawesome@users.noreply.github.com>
2021-12-06 10:31:26 -0500

    Updates

--- precache-manifest.33778c38e3c891d675f66cc8f0d6d1e3.js
+++ precache-manifest.33778c38e3c891d675f66cc8f0d6d1e3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2405750a1c7a74bc7aba6358c50ff9ae",
+    "revision": "8dd231690f6a4618cc6b2dbaec56fa33",
     "url": "/anxious-notes/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/anxious-notes/static/css/2.5510c45e.chunk.css"
   },
   {
-    "revision": "8a2768fc4503420b0319",
-    "url": "/anxious-notes/static/css/main.f38963d9.chunk.css"
+    "revision": "39dec1e7546a13913b78",
+    "url": "/anxious-notes/static/css/main.40dd9a5b.chunk.css"
   },
   {
     "revision": "9ad9de1088f9d326b93f",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/anxious-notes/static/js/2.b6db0f63.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8a2768fc4503420b0319",
-    "url": "/anxious-notes/static/js/main.9cd026e5.chunk.js"
+    "revision": "39dec1e7546a13913b78",
+    "url": "/anxious-notes/static/js/main.140a252b.chunk.js"
   },
   {
     "revision": "72a6bc30271d9e178f9e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anxious-notes/precache-manifest.abda6166519135c2ccb3842314156d50.js"
+  "/anxious-notes/precache-manifest.33778c38e3c891d675f66cc8f0d6d1e3.js"
 );
 
 self.addEventListener('message', (event) => {
